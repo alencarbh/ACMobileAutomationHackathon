83e877d2ed4405e6fc0b162e794b64cbf24d277a
Alencar Barbosa <alencarbh@gmail.com>
2016-08-01 15:01:11 -0300

    adjustments in project structure and layout

--- app/src/main/java/com/alencarbh/android/automationsample/SplashScreen.java
+++ app/src/main/java/com/alencarbh/android/automationsample/SplashScreen.java
@@ -2,33 +2,28 @@ package com.alencarbh.android.automationsample;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
 
-        Thread timerThread = new Thread(){
-            public void run(){
-                try{
-                    sleep(3000);
-                }catch(InterruptedException e){
-                    e.printStackTrace();
-                }finally{
-                    Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
-                    startActivity(intent);
-                }
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
             }
-        };
-        timerThread.start();
+        }, 2000);
     }
 
     @Override
     protected void onPause() {
-        // TODO Auto-generated method stub
         super.onPause();
         finish();
     }
